8dc3024c92fa8cc29be77136ca6843a2d059dd55
fabriciomarote <fabrii.cai93@gmail.com>
2022-09-21 15:02:43 -0300

    cambios por excepcion y otros

--- backend/src/main/java/ar/edu/unq/turnero/modelo/exception/CampoVacioException.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unq.turnero.modelo.exception
-
-class CampoVacioException : Exception() {
-
-    override val message: String?
-        get() = "No se puede crear con un campo vacío."
-
-}
