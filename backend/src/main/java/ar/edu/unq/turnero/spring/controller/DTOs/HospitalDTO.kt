b2c4cca4649bc441e68979c539cb31b4907a23b7
fabriciomarote <fabrii.cai93@gmail.com>
2022-09-21 23:07:23 -0300

    Se modifica HospitalDTO en backend

--- backend/src/main/java/ar/edu/unq/turnero/spring/controller/DTOs/HospitalDTO.kt
+++ backend/src/main/java/ar/edu/unq/turnero/spring/controller/DTOs/HospitalDTO.kt
@@ -2,13 +2,14 @@ package ar.edu.unq.turnero.spring.controller.DTOs
 
 import ar.edu.unq.turnero.modelo.Especialidad
 import ar.edu.unq.turnero.modelo.Hospital
+import ar.edu.unq.turnero.modelo.exception.EspecialidadVacioException
 
 class HospitalDTO(
     var id: Long?,
     var nombre:String?,
     var municipio: String?,
     var direccion: String?,
-    //var especialidades: MutableList<String>
+    var especialidades: List<String>
 ) {
 
     companion object {
@@ -18,10 +19,11 @@ class HospitalDTO(
                 nombre = hospital.nombre,
                 municipio = hospital.municipio,
                 direccion = hospital.direccion,
-                /*especialidades = hospital.especialidades
-                    .map { especialidad -> EspecialidadDTO.desdeModelo(especialidad)}
-                    .toCollection(HashSet()).toMutableList(),
-            */)
+                especialidades = hospital.especialidades
+                    .map { especialidad -> especialidad.toString()}
+                    .toCollection(HashSet()).toList(),
+            )
+
     }
 
     fun aModelo(): Hospital {
@@ -30,12 +32,35 @@ class HospitalDTO(
         hospital.nombre = this.nombre!!
         hospital.municipio = this.municipio
         hospital.direccion = this.direccion!!
-        /*hospital.especialidades = (this.especialidades
-            ?.map { EspecialidadDTO  -> EspecialidadDTO.aModelo()}?.
-            toCollection(mutableListOf<Especialidad>())
-            ?: mutableListOf<Especialidad>())*/
-
+        hospital.especialidades = transformEspecialidades(this.especialidades!!)
         return hospital
+    }
+
+    private fun transformEspecialidades( especialidades: List<String> ) : MutableList<Especialidad> {
+        var nuevasEspecialidades : MutableList<Especialidad> = mutableListOf()
+         especialidades.forEach { especialidad -> nuevasEspecialidades += toEnum(especialidad) }
+
+        return nuevasEspecialidades
+    }
+
+     private fun toEnum(especialidad: String?): Especialidad {
+        var nuevaEspecialidad: Especialidad
+        when(especialidad) {
+            "Pediatria" -> nuevaEspecialidad = Especialidad.PEDIATRIA
+            "Oncologia" -> nuevaEspecialidad = Especialidad.ONCOLOGIA
+            "Traumatologia" -> nuevaEspecialidad = Especialidad.TRAUMATOLOGIA
+            "Urologia" -> nuevaEspecialidad = Especialidad.UROLOGIA
+            "Oftalmologia" -> nuevaEspecialidad = Especialidad.OFTALMOLOGIA
+            "Kinesiologia" -> nuevaEspecialidad = Especialidad.KINESIOLOGIA
+            "Cardiologia" -> nuevaEspecialidad = Especialidad.CARDIOLOGIA
+            "Nefrologia" -> nuevaEspecialidad = Especialidad.NEFROLOGIA
+            "Reumatologia" -> nuevaEspecialidad = Especialidad.REUMATOLOGIA
+            "Dermatologia" -> nuevaEspecialidad = Especialidad.DERMATOLOGIA
+            else -> {
+                throw EspecialidadVacioException()
+            }
+        }
+        return nuevaEspecialidad
     }
 }
 
