36d5a9c5f6ee7787a2f534df86f3248c84d1138a
fabriciomarote <fabrii.cai93@gmail.com>
2022-10-14 13:48:47 -0300

    modificaciones en Home para validar busqueda, terminado

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,29 +1,31 @@
 import React, { useState }  from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from "react-icons/ai";
 import '../styles/Home.css';
 
 const Home = () => {
+
+    const [selection, setSelection] = useState(""); 
+    const [search, setSearch] = useState("");
+    const [showAlert, setShowAlert] = useState (false)
+    const changeSearch = (event) => setSearch(event.target.value);
     const navigate = useNavigate();
+
+    const changeSelectValue = (value) => {
+        setSelection(value)
+        setShowAlert(false)
+    }
 
     const goSearchResults = () => {
         navigate("/hospital/search?q=" + search + "&value=" + selection) ;
     };
 
     const handleButtonClick = () => {
-        if (selection == "") {setShowAlert(true)}
-        else {goSearchResults()}
-    }
-
-    const [selection, setSelection] = useState(""); 
-    const [search, setSearch] = useState("");
-    const changeSearch = (event) => setSearch(event.target.value);
-    const [disable, setDisable] = useState (true)
-    const [showAlert, setShowAlert] = useState (false)
-
-    const changeSelectValue = (value) => {
-        setSelection(value)
-        setShowAlert(false)
+        if (selection === "") {
+            setShowAlert(true)
+        } else {
+            goSearchResults()
+        }
     }
 
     return (
@@ -34,17 +36,21 @@ const Home = () => {
                 </div>
                 <div className="search-content">  
                     <div className='search-bar'>
-                        { showAlert ? <div className="alert alert-danger" role="alert">ERROR</div> : ""}
-                        <input className="search-btn" type="text" placeholder="Buscar hospital" aria-label="Search" onChange={changeSearch} />
-                        <button onClick={() => handleButtonClick(search, selection)} className="btn btn-secondary" type="submit"><AiOutlineSearch color="white" size={25}/></button>
+                        <div className='message-error'>
+                            { showAlert ? <div className="alert alert-success" role="alert">Debe seleccionar una opción para realizar la busqueda</div> : ""}
+                        </div>
+                        <div className='box-search'>
+                            <input className="search-btn" type="text" placeholder="Buscar hospital" aria-label="Search" onChange={changeSearch} />
+                            <button onClick={() => handleButtonClick(search, selection)} className="btn btn-secondary" type="submit"><AiOutlineSearch color="white" size={25}/></button>
+                        </div>
                     </div>
                     <div className='text-2'>
                         
                     </div>
                     <div className='search-checkbox'>
                         <input type="radio" className="radio" name="1" onClick={ () => changeSelectValue("nombre")} required/><label className='checkbox'>Nombre</label>
-                        <input type="radio" className="radio" name="2" onClick={ () => changeSelectValue("municipio")} required/><label className='checkbox'>Municipio</label>
-                        <input type="radio" className="radio" name="3" onClick={ () => changeSelectValue("especialidad")} required/><label className='checkbox'>Especialidad</label>
+                        <input type="radio" className="radio" name="1" onClick={ () => changeSelectValue("municipio")} required/><label className='checkbox'>Municipio</label>
+                        <input type="radio" className="radio" name="1" onClick={ () => changeSelectValue("especialidad")} required/><label className='checkbox'>Especialidad</label>
                     </div>
                 </div>
             </div>
--- frontend/src/components/Turno.jsx
+++ frontend/src/components/Turno.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
-import Service from '../Service/service';
+import Service from '../service/service';
 
 const Turno = () => {
 
