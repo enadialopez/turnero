80c3a845e564c49f90bad5eea0b0992a9b47d162
fabriciomarote <fabrii.cai93@gmail.com>
2022-10-22 00:57:43 -0300

    Se agregan request necesarios y se refactoriza UsuarioController.kt

--- backend/src/main/java/ar/edu/unq/turnero/spring/controller/UsuarioController.kt
+++ backend/src/main/java/ar/edu/unq/turnero/spring/controller/UsuarioController.kt
@@ -5,22 +5,28 @@ import ar.edu.unq.turnero.spring.controller.DTOs.MiniUsuarioDTO
 import ar.edu.unq.turnero.spring.controller.DTOs.UsuarioDTO
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import javax.servlet.http.HttpServletResponse
 
 @RestController
-@CrossOrigin(maxAge = 3600, allowCredentials = "true")
+@CrossOrigin
 @RequestMapping("/usuario")
 class UsuarioController(private val usuarioService: UsuarioService) {
 
     @PostMapping("/register")
     fun register(@RequestBody usuario: UsuarioDTO, response: HttpServletResponse) : ResponseEntity<Any> {
-        usuarioService.crear(usuario.aModelo())
-
-        response.setHeader("Access-Control-Expose-Headers", "*");
-
+        val user = usuarioService.crear(usuario.aModelo())
+        val issuer = user!!.id.toString()
+        val jwt = Jwts.builder()
+            .setIssuer(issuer)
+            .setExpiration(Date(System.currentTimeMillis() + 60 * 24 * 2000))
+            .signWith(SignatureAlgorithm.HS512, "secret").compact()
+        response.addHeader("Authorization", jwt)
+        user.token = jwt
+        usuarioService.actualizar(user)
         return ResponseEntity.ok().body("Ok")
     }
 
@@ -28,21 +34,22 @@ class UsuarioController(private val usuarioService: UsuarioService) {
     fun login(@RequestBody usuario: MiniUsuarioDTO, response: HttpServletResponse) : ResponseEntity<Any> {
         val user = usuarioService.recuperarPorEmail(usuario.email!!)
         val issuer = user!!.id.toString()
-
         val jwt = Jwts.builder()
             .setIssuer(issuer)
-            .setExpiration(Date(System.currentTimeMillis() + 60 * 24 * 1000))
+            .setExpiration(Date(System.currentTimeMillis() + 60 * 24 * 2000))
             .signWith(SignatureAlgorithm.HS512, "secret").compact()
-
-        response.setHeader("Authorization", jwt);
-        response.setHeader("Access-Control-Expose-Headers", "*");
-
-        return ResponseEntity.ok().body(user)
+        response.addHeader("Authorization", jwt)
+        user.token = jwt
+        val userResponse = usuarioService.actualizar(user)
+        return ResponseEntity.ok().body(userResponse)
     }
 
-    @GetMapping("/{usuariolId}")
-    fun recuperar(@PathVariable usuarioId: Long) = UsuarioDTO.desdeModelo(usuarioService.recuperar(usuarioId.toInt())!!)
-
     @GetMapping("")
+    fun recuperar(@RequestHeader(HttpHeaders.AUTHORIZATION) token: String  ) : ResponseEntity<Any> {
+        val user = usuarioService.recuperarPorToken(token)
+        return ResponseEntity.ok().body(UsuarioDTO.desdeModelo(user!!))
+    }
+
+    @GetMapping("/todos")
     fun recuperarTodos() = usuarioService.recuperarTodos().map { usuario -> UsuarioDTO.desdeModelo(usuario)  }
 }
