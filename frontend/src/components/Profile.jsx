f979433e278df5ee9bc54bfe35ac37846404590f
enadialopez <enadialopez@gmail.com>
2022-11-03 06:26:10 -0300

    agrego editar perfil

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,13 +1,29 @@
-import React, { useState, useEffect }  from 'react';
-import { useNavigate } from "react-router-dom";
-import Navbar from '../components/Navbar';
-import Service from '../service/service';
-import TurnoModel from './TurnoModel';
-import axios from 'axios';
-import '../styles/Profile.css';
+import React, { useState, useEffect }  from 'react'
+import { useNavigate } from "react-router-dom"
+import Navbar from '../components/Navbar'
+import Service from '../service/service'
+import TurnoModel from './TurnoModel'
+import {Modal, TextField, Button} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import axios from 'axios'
+import '../styles/Profile.css'
+
+const useStyles=makeStyles((theme)=>({
+  modal: {
+    position:'absolute',
+    width: 400,
+    backgroundColor: 'white',
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: "16px 32px 24px",
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}))
 
 const Profile = () => {
-    const [turnos, setTurnos] = useState([]);
+    const [turnos, setTurnos] = useState([])
     const [user, setUser] = useState({
         id: "",
         nombreYApellido: "",
@@ -16,11 +32,22 @@ const Profile = () => {
         email: "",
         telefono: "",
         password: "",
-    });
-    const navigate = useNavigate();
-    const isLogged = !!localStorage.getItem("token");
+    })
+    const navigate = useNavigate()
+    const isLogged = !!localStorage.getItem("token")
 
-    axios.defaults.headers['authorization'] = localStorage.getItem('token');
+    axios.defaults.headers['authorization'] = localStorage.getItem('token')
+
+    const handleChange = name => event => {
+      setUser(prevState => ({ ...prevState, [name]: event.target.value }));
+  };
+
+    const styles = useStyles()
+
+    const [modal, setModal]=useState(false)
+    const abrirCerrarModal=()=> {
+      setModal(!modal)
+    }
 
     useEffect(() => {
         if (isLogged){
@@ -63,6 +90,24 @@ const Profile = () => {
       });
   };
 
+  const body=(
+    <div className={styles.modal}>
+      <div align="center">
+        <h2>Editar perfil</h2>
+      </div>
+      <TextField label= "Nombre y Apellido" className = {styles.textfield} value={user.nombreYApellido} onChange={handleChange("nombreYApellido")} />
+      <br/> <br/>
+      <TextField label= "Telefono" className = {styles.textfield} value={user.telefono} onChange={handleChange("telefono")} />
+      <br/> <br/>
+      <TextField label= "email" className = {styles.textfield} value={user.email} onChange={handleChange("email")} />
+      <br/> <br/>
+      <div align="right">
+        <Button onClick={()=>abrirCerrarModal()} color="primary">Aceptar</Button>
+        <Button onClick={()=>abrirCerrarModal()}>Cancelar</Button>
+      </div>
+    </div>
+  )
+
     return (
         <>
             <div className="navbar">
@@ -79,8 +124,18 @@ const Profile = () => {
                           <div className='info'><p className='info-title'>DNI: </p>{user.dni}</div>
                           <div className='info'><p className='info-title'>Telefono: </p>{user.telefono}</div>
                           <div className='info'><p className='info-title'>Email: </p>{user.email}</div>
+
                         </div> 
+                        
                         <div className='box-right col-lg-8 col-md-8 col-sm-8 col-xs-8'>
+                          <div className={styles.button}>
+                            <Button className={styles.button} onClick={()=>abrirCerrarModal()} >Editar perfil</Button>
+                              <Modal
+                                open={modal}
+                                onClose={abrirCerrarModal}>
+                                    {body}
+                                    </Modal>
+                                    </div>
                           <button type="button" className="button-profile" id="modal" data-toggle="modal" data-target="#exampleModalCenter">Cerrar cuenta</button>
                             <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                               <div className="modal-dialog modal-dialog-centered" role="document">
@@ -89,6 +144,7 @@ const Profile = () => {
                                       <p className="ask" id="exampleModalLongTitle">¿Estas segura/o de cerrar tu cuenta?</p>
 
                                     </div>
+                                  
                                     <div className="modal-body-profile">
                                     Si dá de baja su cuenta, los turnos asignados vuelven a estar como diponibles
                                       <button className="btn-info b-profile" id='confirm' onClick={() => deleteAccount()}>Si, cerrar cuenta</button>
