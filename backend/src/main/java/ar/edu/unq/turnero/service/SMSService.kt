65d18ebba9c426c59308c23ddff90f6253acd2a8
fabriciomarote <fabrii.cai93@gmail.com>
2022-11-07 20:02:08 -0300

    Se actualiza servicio de SMS y se saca acreditaciones

--- backend/src/main/java/ar/edu/unq/turnero/service/SMSService.kt
+++ backend/src/main/java/ar/edu/unq/turnero/service/SMSService.kt
@@ -2,6 +2,7 @@ package ar.edu.unq.turnero.service
 
 import ar.edu.unq.turnero.modelo.SMS
 import com.twilio.Twilio
+import ar.edu.unq.turnero.service.TwilioAccount
 import com.twilio.rest.api.v2010.account.Message
 import com.twilio.type.PhoneNumber
 import org.springframework.stereotype.Component
@@ -11,15 +12,12 @@ import java.net.URI
 @Component
 class SMSService {
 
-    private val ACCOUNT_SID: String? = "AC268d0b045d2b3e270fc1f1ff55f8e84b"
-    private val AUTH_TOKEN: String? = "68121216ded1e01170053200ba7314e9"
-    private val FROM_NUMBER: String? = "+14634002242"
-
+    private val twilioAccount = TwilioAccount()
     fun send(sms: SMS) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN)
+        Twilio.init(twilioAccount.ACCOUNT_SID, twilioAccount.AUTH_TOKEN)
         // to number, from: always twilio snd a message
         println(sms.to)
-        val message: Message = Message.creator(PhoneNumber(sms.to), PhoneNumber(FROM_NUMBER), sms.message)
+        val message: Message = Message.creator(PhoneNumber(sms.to), PhoneNumber(twilioAccount.FROM_NUMBER), sms.message)
             .setStatusCallback(URI.create("http://677add1a.ngrok.io/smscallback"))
             .create()
     }
